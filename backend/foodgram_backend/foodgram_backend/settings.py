659dece4769eae0337092eb037c1701a10691f86
Anastasiya Cherkasova <cherkasovaam@bk.ru>
2023-09-08 02:07:51 +0300

    user work for sure!~

--- backend/foodgram_backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/foodgram_backend/settings.py
@@ -148,6 +148,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_PAGINATION_CLASS': ('api.paginators.'
+                                 'PageLimitPagination'),
+    "PAGE_SIZE": 10,
 }
 
 # SIMPLE_JWT = {
