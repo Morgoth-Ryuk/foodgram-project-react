a01cf5b1575064ed2a2e19a4743e185e823de85b
Anastasiya Cherkasova <cherkasovaam@bk.ru>
2023-09-13 00:33:02 +0300

    заменяю несколько одинаковых укороченыз сериализаторов рецепта в оди

--- backend/foodgram_backend/api/serializers.py
+++ backend/foodgram_backend/api/serializers.py
@@ -79,16 +79,16 @@ class IngredientSerializer(ModelSerializer):
         read_only_fields = ('__all__',)
 
 
-class RecipeInCartSerializer(ModelSerializer):
-    class Meta:
-        model = Recipe
-        fields = (
-            'id',
-            'name',
-            'image',
-            'cooking_time'
-        )
-        read_only_fields = ('__all__',)
+# class RecipeInCartSerializer(ModelSerializer):
+#     class Meta:
+#         model = Recipe
+#         fields = (
+#             'id',
+#             'name',
+#             'image',
+#             'cooking_time'
+#         )
+#         read_only_fields = ('__all__',)
 
 
 class FavoriteRecipeSerializer(ModelSerializer):
