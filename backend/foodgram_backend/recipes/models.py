3475e7b13965bd498bdd7b44238cfda9bacaa248
Anastasiya Cherkasova <cherkasovaam@bk.ru>
2023-09-12 16:39:25 +0300

    little changes for max value

--- backend/foodgram_backend/recipes/models.py
+++ backend/foodgram_backend/recipes/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from users.models import User
 from django.core.validators import MinValueValidator
-from django.core.validators import MaxValueValidator
+# from django.core.validators import MaxValueValidator
 
 
 class Tag(models.Model):
