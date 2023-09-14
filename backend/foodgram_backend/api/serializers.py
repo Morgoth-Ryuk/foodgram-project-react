283078175a2a1978d646ee94e094b1551f440f41
Anastasiya Cherkasova <cherkasovaam@bk.ru>
2023-09-14 20:31:59 +0300

    поправила get_is_sub

--- backend/foodgram_backend/api/serializers.py
+++ backend/foodgram_backend/api/serializers.py
@@ -43,7 +43,6 @@ class CustomUserSerializer(UserSerializer):
         request = self.context.get('request')
         if not request or request.user.is_anonymous:
             return False
-            
         return Subscription.objects.filter(
             user=request.user, author=obj
         ).exists()
